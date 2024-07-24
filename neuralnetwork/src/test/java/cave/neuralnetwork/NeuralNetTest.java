2284188f67cd0405809bc4f63b7f6b00c8e3a15d
Helen Ma <hhma@alumni.cpp.edu>
2024-07-23 21:10:40 -0400

    add test method.

--- neuralnetwork/src/test/java/cave/neuralnetwork/NeuralNetTest.java
+++ neuralnetwork/src/test/java/cave/neuralnetwork/NeuralNetTest.java
@@ -322,4 +322,23 @@ class NeuralNetTest {
 
 	}
 
+
+	@Test
+	void test(){
+		Matrix input = new Matrix (3,3, i->(i+1));
+		Matrix weight = new Matrix (3,3,i-> (i+1));
+		Matrix biases = new Matrix(3,1, i-> (i+1));
+
+		Matrix result = weight.multiply(input).modify((row, col, value) -> value + biases.get(row));
+
+		double[] expectedValues ={ +31.00000, +37.00000, +43.00000, +68.00000, +83.00000, +98.00000, +105.00000, +129.00000, +153.00000 };
+		Matrix expected = new Matrix(3,3, i->expectedValues[i]);
+
+		assertTrue(expected.equals(result));
+		System.out.println(input);
+		System.out.println(weight);
+		System.out.println(biases);
+		System.out.println(result);
+
+	}
 }
